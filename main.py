2b70ef215327f833a5cc8b7ed630867903737d77
irpanpilihanrambe <irpanpilihann@gmail.com>
2026-05-01 19:18:33 +0700

    Fix NameError: add asynccontextmanager import

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from contextlib import asynccontextmanager
 
 # Tambahkan ini untuk deteksi folder secara otomatis
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
